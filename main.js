c61e9d50733973d1037bde987db48ccab9bb9531
Alfonso Madrid <amadrida@bch.bancodechile.cl>
2023-06-03 07:25:40 -0400

    add storage dark-mode

--- main.js
+++ main.js
@@ -15,7 +15,21 @@ const btnMode = document.querySelector('#switch')
 btnMode.addEventListener('click', () => {
 	document.body.classList.toggle('dark')
 	btnMode.classList.toggle('active')
-})
+
+	if(document.body.classList.contains('dark')) {
+		localStorage.setItem('dark-mode', 'true');
+	}else{
+		localStorage.setItem('dark-mode', 'false');
+	}
+});
+
+if(localStorage.getItem('dark-mode') === 'true') {
+	document.body.classList.add('dark');
+	btnMode.classList.add('active')
+}else{
+	document.body.classList.remove('dark');
+	btnMode.classList.remove('active')
+}
 
 const regexInput = {
 	number: /^\d+$/,
